117d7f11a161ea4f52b5ad9c76e72320d7a3e99c
MrGuidry <33229556+MrGuidry@users.noreply.github.com>
2019-01-30 17:24:43 -0800

    Add files via upload
    Program to print out "Hello, my name is Ryan Guidry"

--- hwOne.c
+++ hwOne.c
@@ -1,11 +1,11 @@
 /*
  * hwOne.c
  *
- *  Created on: Jan 30, 2019
+ *  Created on: January 30, 2019
  *      Author: Ryan Guidry
  */
 #include <stdio.h>
 
 void main(int argc, char **argv){
-	printf("Hello World");
+	printf("Hello, my name is Ryan Guidry");
 }
