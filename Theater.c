a4d56fb1415ff870895d4fc44b693a6a51c72fff
MrGuidry <33229556+MrGuidry@users.noreply.github.com>
2019-02-25 14:27:27 -0800

    Adding Comments

--- Theater.c
+++ Theater.c
@@ -1,8 +1,8 @@
 /*
  * Theater.c
  *
- *  Created on: Feb 25, 2019
- *      Author: baseb
+ *  Created on: February 25, 2019
+ *      Author: Ryan Guidry
  */
 #include <stdio.h>
 #include <stdbool.h>
@@ -17,9 +17,9 @@ int getSeatsRow(char row[]);
 int getSeatsSold(char theater[ROWS][COLUMNS]);
 double buyTickets(char row[], double price);
 
-
+/* Main Method for the Program */
 void Theater(){
-	char theater[15][30] =
+	char theater[15][30] =					//Creation of the theater with '#' in every seat
 		{
 			"##############################",
 			"##############################",
@@ -42,70 +42,72 @@ void Theater(){
 	double rowPrices[15];
 	int size = sizeof(rowPrices)/sizeof(rowPrices[0]);
 	makePrices(rowPrices, size);
-
-	double ticketSales = 0.0;
+	
+	double ticketSales = 0.0; 				//Declaration and Assignment of the variable used o track the total sales
+	
 	//Main Menu
 	int choice;
 	bool done = false;
-		while(!done){
-
-			printf("1. Buy Tickets.\n");
-			printf("2. How many seats have been sold? \n");
-			printf("3. How many seats are available in each row? \n");
-			printf("4. How many seats are available in the auditorium? \n");
-			printf("5. View Ticket Sales\n");
-			printf("6. Quit\n");
-			printf("Enter your choice (1-6)\n");
-			fflush(stdout);
-			scanf("%d", &choice);
-				switch(choice){
-				case 1://Buy tickets.
-					displayContents(theater);
-					int row = 0;
-					int valid = false;
-					while(!valid){
-						printf("Which row would like to purchase tickets in? (1-15)");
-						fflush(stdout);
-						scanf("%d", &row);
-						if(row >= 1 && row <= 15){
-							if(getSeatsRow(theater[row-1]) != 0){
-								ticketSales += buyTickets(theater[row-1], rowPrices[row-1]);
-								valid = true;
-							}else{
-								printf("All seats in that row are sold out!!!\n");
-							}
-
+	while(!done){
+		printf("1. Buy Tickets.\n");
+		printf("2. How many seats have been sold? \n");
+		printf("3. How many seats are available in each row? \n");
+		printf("4. How many seats are available in the auditorium? \n");
+		printf("5. View Ticket Sales\n");
+		printf("6. Quit\n");
+		printf("Enter your choice (1-6)\n");
+		fflush(stdout);
+		scanf("%d", &choice);
+			switch(choice){
+			case 1://Buy tickets
+				displayContents(theater);
+				int row = 0;
+				int valid = false;
+				while(!valid){
+					printf("Which row would like to purchase tickets in? (1-15)");
+					fflush(stdout);
+					scanf("%d", &row);
+					if(row >= 1 && row <= 15){ //Checking to make sure the row choice is from 1 to 15
+						if(getSeatsRow(theater[row-1]) != 0){ //Checking to make sure there are available seats in the row chosen
+							ticketSales += buyTickets(theater[row-1], rowPrices[row-1]);
+							valid = true;
 						}else{
-							printf("Invalid Row Number!!!\n");
+							printf("All seats in that row are sold out!!!\n");
 						}
-
+					}else{
+						printf("Invalid Row Number!!!\n");
 					}
-					displayContents(theater);
-					break;
-				case 2://How many seats have been sold?
-					printf("Seats Sold: %d\n", getSeatsSold(theater));
-					break;
-				case 3://How many seats are available in each row?
-					for(int r = 0; r < ROWS;r++){
-						printf("Seats Available in Row %d: %d\n", (r+1), getSeatsRow(theater[r]));
-					}
-					break;
-				case 4://How many seats available in the auditorium
-					printf("Seats Available: %d\n", getSeatsAuditorium(theater));
-					break;
-				case 5://View Ticket Sales
-					printf("Ticket Sales: $%.2f\n", ticketSales);
-					break;
-				case 6:
-					done = true;
-					break;
-				default:
-					printf("Error! Incorrect choice.\n");
 				}
-		}
-
+				displayContents(theater);
+				break;
+			case 2://How many seats have been sold?
+				printf("Seats Sold: %d\n", getSeatsSold(theater));
+				break;
+			case 3://How many seats are available in each row?
+				for(int r = 0; r < ROWS;r++){
+					printf("Seats Available in Row %d: %d\n", (r+1), getSeatsRow(theater[r]));
+				}
+				break;
+			case 4://How many seats available in the auditorium
+				printf("Seats Available: %d\n", getSeatsAuditorium(theater));
+				break;
+			case 5://View Ticket Sales
+				printf("Ticket Sales: $%.2f\n", ticketSales);
+				break;
+			case 6://QUIT
+				done = true;
+				break;
+			default:
+				printf("Error! Incorrect choice.\n");
+			}
+	}
 }
 
+/* BuyTickets
+ * This method is used to allow the user to choose how many tickets they want to buy
+ * as well as what seat they want to buy. This method updated the contents on valid choices
+ * and tallies up the total cost. Returns: total cost of tickets.
+ */
 double buyTickets(char row[], double price){
 	int numberOfTickets;
 	bool valid = false;
@@ -114,7 +116,7 @@ double buyTickets(char row[], double price){
 		printf("How many tickets would you like to buy?");
 		fflush(stdout);
 		scanf("%d", &numberOfTickets);
-		if(numberOfTickets >= 0 && numberOfTickets <= getSeatsRow(row)){
+		if(numberOfTickets >= 0 && numberOfTickets <= getSeatsRow(row)){ //Checks to make sure the amount of tickets to buy is not negative as well as less than the amount of seats left in that row
 			valid = true;
 		}else{
 			printf("Not enough seats in the row!!!\n");
@@ -129,7 +131,7 @@ double buyTickets(char row[], double price){
 			fflush(stdout);
 			scanf("%d", &choice);
 			if(choice >= 1){
-				if(row[choice-1] == '*'){
+				if(row[choice-1] == '*'){//If the seat they chose is taken, then it will tell them. If not taken, then seat is updated and total price is updated.
 					printf("This seat is already taken!!!\n");
 				}else{
 					row[choice-1] = '*';
@@ -141,10 +143,15 @@ double buyTickets(char row[], double price){
 			}
 		}
 	}
-	printf("Total Price: $%.2f\n", totalPrice);
-	return totalPrice;
+	printf("Total Price: $%.2f\n", totalPrice); //Displays total Cost of the culmination of tickets
+	return totalPrice; //Returning the total price of all the tickets to be used outside this method
 }
 
+/* GetSeatsSold
+ * This method goes through the theater and increments a variable whenever
+ * a seat is comes across has been sold.
+ * Returns: numbers of seats sold.
+ */
 int getSeatsSold(char theater[ROWS][COLUMNS]){
 	int sold = 0;
 	for(int r = 0; r < ROWS; r++){
@@ -156,6 +163,11 @@ int getSeatsSold(char theater[ROWS][COLUMNS]){
 	return sold;
 }
 
+/* GetSeatsRow
+ * This method goes through a certain row specified in the parameter
+ * and increments a variable whenever it comes across a seat that is available. 
+ * Returns: Total number of available seats in that row.
+ */
 int getSeatsRow(char row[]){
 	int available = 0;
 	for(int c = 0; c < COLUMNS; c++){
@@ -166,6 +178,11 @@ int getSeatsRow(char row[]){
 	return available;
 }
 
+/* GetSeatsAuditorium
+ * This method goes through the entire theater and increments a 
+ * variable whenever it comes across a seat that is available. 
+ * Returns: Total number of available seats in the theater.
+ */
 int getSeatsAuditorium(char theater[ROWS][COLUMNS]){
 	int available = 0;
 	for(int r = 0; r < ROWS; r++){
@@ -177,6 +194,10 @@ int getSeatsAuditorium(char theater[ROWS][COLUMNS]){
 	return available;
 }
 
+/* MakePrices
+ * This method takes in an array that will be used in the method and the size of the array.
+ * In this method, the user specifies what prices they want each row to be for every seat in that row.
+ */
 void makePrices(double array[], int size){
 	int index = 0;
 	for(int x = 1; x <= size; x++){
@@ -185,7 +206,7 @@ void makePrices(double array[], int size){
 			printf("Enter the price for Row %d:", x);
 			fflush(stdout);
 			scanf("%lf", (array+index));
-			if(array[index] >= 0){
+			if(array[index] >= 0){//Check to make sure the value specified is greater than or equal to 0
 				valid = true;
 			}else{
 				printf("Please enter a positive Price!!!\n");
@@ -194,6 +215,12 @@ void makePrices(double array[], int size){
 		index++;
 	}
 }
+
+/* Display Contents
+ *
+ * This method displays the entire theater in such a way secified by the program prompt
+ *
+ */
 
 void displayContents(char theater[ROWS][COLUMNS]){
 	printf("                      Seats\n");
